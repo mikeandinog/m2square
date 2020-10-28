1e8d0960b0c99f6176cd32e0c5a2a3c6f88e02f5
Michael Andino <mikeandinog@Michaels-MBP.attlocal.net>
2020-10-28 18:34:09 -0400

    first commit

---
+++ src/web/static/js/main.js
@@ -0,0 +1,45 @@
+/*! main.js | Bulkit | CSS Ninja */
+
+/* ==========================================================================
+Website core JS file
+========================================================================== */
+"use strict"; //Set environment variable (Used for development and demo)
+
+/* 
+    Possible values:
+    1. development
+    2. demo
+    3. customization
+*/
+
+var env = 'customization';
+initPageLoader();
+$(document).ready(function ($) {
+  if (env === 'development') {
+    disableSidebarLinks();
+    changeDemoImages();
+  } else if (env === 'demo') {
+    changeDemoImages();
+  } else if (env === 'customization') {
+    disableSidebarLinks();
+  }
+
+  initNavbar();
+  initMobileMenu();
+  initSlider();
+  initParallax();
+  initMockup();
+  initSidebar();
+  initCounters();
+  initScrollReveal();
+  initVideoEmbed();
+  initVerticalCarousel();
+  initPopovers();
+  initTooltips();
+  initBackgroundImages();
+  initBackToTop();
+  initGitem();
+  initScrollToHash();
+  highlightNavbarLinks();
+  initGoogleMap();
+});
